e218d2303b4f2a828291493000b13f5b159eb4c2
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2020-02-18 09:30:17 -0500

    Change class from .card-title to .card-heading

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const Card = () => (
   <div className="card">
-    <h4 className="card-title">I'm a Card Title</h4>
+    <h4 className="card-heading">I'm a Card Heading</h4>
     <Image />
     <p className="card-text text--sm">
       Card text is a paragraph element with a class of card dash text.
